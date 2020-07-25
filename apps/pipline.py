661633650cac3389551c08a6bf2cb716b275b516
Sau Yee Chan <15137352+y26805@users.noreply.github.com>
2020-07-25 21:11:49 +0900

    Remove unused var

--- apps/pipline.py
+++ apps/pipline.py
@@ -33,8 +33,6 @@ MetadatTxt = os.path.join(root, 'metadata')
 if not os.path.exists(MetadatTxt):
     os.mkdir(MetadatTxt)
 
-
-monthlist = ['11', '12', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
 metalib.GSVpanoMetadataCollectorBatch_Yaw_fiona(outshp, 1000, MetadatTxt)
 print('Collecting metadata')
 
